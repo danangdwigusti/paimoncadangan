0b7d8136475f927038b36f6c250617ce0208feae
danangdwigusti <108175118+danangdwigusti@users.noreply.github.com>
2022-07-15 22:56:54 +0700

    Update weapon.js

--- Command/gamef/weapon.js
+++ Command/gamef/weapon.js
@@ -2,8 +2,15 @@ const Discord = require("discord.js");
 const { MessageEmbed } = require('discord.js');
 exports.run = (client, msg, args) => {
     
-    
-    if (msg.content === "!weapon akuomaru") { //cursed
+    if (msg.content === "!weapon kagotsurube") { //cursed
+      const exampleEmbed = new MessageEmbed()
+      .addField(":crossed_swords:  Weapon Information :crossed_swords:", "Paimon ready for `!help` >.<", true)
+      .setImage('https://drive.google.com/uc?id=1pWtBIRWOLcFs7wd-rHk3LIhDcMdR2Trc')
+      .setTimestamp()
+      .setFooter({ text: 'Genshination',
+      iconURL: 'https://s3.getstickerpack.com/storage/uploads/sticker-pack/genshin-impact-paimon-2/tray_large.png?41ad332a85dc0a0fbe8c0f922eae5097'});
+    msg.reply({ embeds: [exampleEmbed] });
+   } else if (msg.content === "!weapon cursed blade") { //cursed
       const exampleEmbed = new MessageEmbed()
       .addField(":crossed_swords:  Weapon Information :crossed_swords:", "Paimon ready for `!help` >.<", true)
       .setImage('https://drive.google.com/uc?id=1pWtBIRWOLcFs7wd-rHk3LIhDcMdR2Trc')
