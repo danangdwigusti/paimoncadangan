299b4de0472ff275308ad49f14aaafd95f7f9108
danangdwigusti <108175118+danangdwigusti@users.noreply.github.com>
2022-08-24 16:04:06 +0700

    Add files via upload

--- Command/gamef/build.js
+++ Command/gamef/build.js
@@ -1,19 +1,11 @@
 const Discord = require("discord.js");
 const { MessageEmbed } = require('discord.js');
 exports.run = (client, msg, args) => {
-  
+
   if (msg.content === "!build albedo") { //albedo
     const exampleEmbed = new MessageEmbed()
     .addField("💫  Character Build 💫", "Paimon ready for `!help` >.<", true)
     .setImage('https://drive.google.com/uc?id=1KMSHvMqen-VFdXMU9FYFz8LjZrIiqbI3')
-    .setTimestamp()
-    .setFooter({ text: 'Genshination',
-    iconURL: 'https://s3.getstickerpack.com/storage/uploads/sticker-pack/genshin-impact-paimon-2/tray_large.png?41ad332a85dc0a0fbe8c0f922eae5097'});
-  msg.reply({ embeds: [exampleEmbed] });
-  } else if (msg.content === "!build heizou") { //heizou
-    const exampleEmbed = new MessageEmbed()
-    .addField("💫  Character Build 💫", "Paimon ready for `!help` >.<", true)
-    .setImage('https://drive.google.com/uc?id=1S3PHVS4KzGJU0JM5jQ_m_OQ048YPSJ-q')
     .setTimestamp()
     .setFooter({ text: 'Genshination',
     iconURL: 'https://s3.getstickerpack.com/storage/uploads/sticker-pack/genshin-impact-paimon-2/tray_large.png?41ad332a85dc0a0fbe8c0f922eae5097'});
@@ -49,7 +41,7 @@ exports.run = (client, msg, args) => {
   else if (msg.content === "!build ayaka") { // AYAKA
     const exampleEmbed = new MessageEmbed()
     .addField("💫  Character Build 💫", "Paimon ready for `!help` >.<", true)
-    .setImage('https://drive.google.com/uc?id=1xDnhnUcsBKTtnuLgoxfHVjIRSXIFxVa1')
+    .setImage('https://drive.google.com/uc?id=10sMjnlUwLjym9VG5ZQSzbojwA78QGxg0')
     .setTimestamp()
     .setFooter({ text: 'Genshination',
     iconURL: 'https://s3.getstickerpack.com/storage/uploads/sticker-pack/genshin-impact-paimon-2/tray_large.png?41ad332a85dc0a0fbe8c0f922eae5097'});
@@ -445,7 +437,7 @@ exports.run = (client, msg, args) => {
   else if (msg.content === "!build xingqiu") { // XINQIU
     const exampleEmbed = new MessageEmbed()
     .addField("💫  Character Build 💫", "Paimon ready for `!help` >.<", true)
-    .setImage('https://drive.google.com/uc?id=1tcEVDVyTXnH9NDvpI_VfIz5Swisfzp6T')
+    .setImage('https://drive.google.com/uc?id=1kLTFhtVx4dLgnm2UvG--txAGgE2x8g3h')
     .setTimestamp()
     .setFooter({ text: 'Genshination',
     iconURL: 'https://s3.getstickerpack.com/storage/uploads/sticker-pack/genshin-impact-paimon-2/tray_large.png?41ad332a85dc0a0fbe8c0f922eae5097'});
