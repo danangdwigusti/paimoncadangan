7c5201ef1088c30b832babffc92c2ae1ed37f607
danangdwigusti <108175118+danangdwigusti@users.noreply.github.com>
2022-08-24 16:07:49 +0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,7 +1,7 @@
 const { MessageEmbed } = require('discord.js');
 const { Routes } = require("discord-api-types/v9")
 const { REST } = require("@discordjs/rest") 
-const { Player } = require("discord-player")
+
 const Userchm = require("./Command/informationf/profileSchema.js");
 const mongoose = require("mongoose");
 const Discord = require("discord.js");
@@ -231,61 +231,6 @@ client.on("messageCreate", msg => {
     command.run(client, msg, args)
   }
 })
-
-//---- Fungsi Musik --------------------------
-const LOAD_SLASH = process.argv[2] == "load" //Load Slash
-const CLIENT_ID = "978615870718869594"
-const GUILD_ID = "981210961014829167"
-
-client.slashcommands = new Discord.Collection()
-client.player = new Player(client, {
-    ytdlOptions: {
-        quality: "highestaudio",
-        highWaterMark: 1 << 25
-    }
-})
-
-let commands = []
-
-const slashFiles = fs.readdirSync("./slash").filter(file => file.endsWith(".js"))
-for (const file of slashFiles){
-    const slashcmd = require(`./slash/${file}`)
-    client.slashcommands.set(slashcmd.data.name, slashcmd)
-    if (LOAD_SLASH) commands.push(slashcmd.data.toJSON())
-}
-
-if (LOAD_SLASH) {
-    const rest = new REST({ version: "9" }).setToken(TOKEN)
-    console.log("Deploying slash commands")
-    rest.put(Routes.applicationGuildCommands(CLIENT_ID, GUILD_ID), {body: commands})
-    .then(() => {
-        console.log("Successfully loaded")
-        process.exit(0)
-    })
-    .catch((err) => {
-        if (err){
-            console.log(err)
-            process.exit(1)
-        }
-    })
-}
-else {
-    client.on("ready", () => {
-        console.log(`Logged in as ${client.user.tag}`)
-    })
-    client.on("interactionCreate", (interaction) => {
-        async function handleCommand() {
-            if (!interaction.isCommand()) return
-
-            const slashcmd = client.slashcommands.get(interaction.commandName)
-            if (!slashcmd) interaction.reply("Not a valid slash command")
-
-            await interaction.deferReply()
-            await slashcmd.run({ client, interaction })
-        }
-        handleCommand()
-    })
-}
 
 //---- Fungsi Registrasi --------------------------
 const subnick = "!reg nickname:";
